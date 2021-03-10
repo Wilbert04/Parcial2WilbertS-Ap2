858919df962dafa759d2cc2a2712292a36a5759d
Wilbert Smith Antigua Castillo <wilbert_antigua@ucne.edu.do>
2021-03-10 13:54:32 -0600

    Models listas

---
+++ SegundoParcial_AP/Models/CobrosDetalle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SegundoParcial_AP.Models
+{
+    public class CobrosDetalle
+    {
+        [Key]
+        public int Id { get; set; }
+        public int CobroId { get; set; }
+        public int VentaId { get; set; }
+        public float Cobrado { get; set; }
+
+        public virtual Cobros Cobro { get; set; }
+        public virtual Ventas Venta { get; set; }
+    }
+}
