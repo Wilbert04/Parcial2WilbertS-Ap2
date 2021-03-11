738c3052aadd1ca8cd3e7b758e6dd8682e602921
Wilbert Smith Antigua Castillo <wilbert_antigua@ucne.edu.do>
2021-03-10 18:58:39 -0600

    Paquetes instalados, Using agregados

---
+++ SegundoParcial_AP/BLL/ClientesBLL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SegundoParcial_AP.BLL
+{
+    public class ClientesBLL
+    {
+
+
+    }
+}
