8647d3af3b45089b4288d7d467fa6eff52374511
Wilbert Smith Antigua Castillo <wilbert_antigua@ucne.edu.do>
2021-03-10 20:10:17 -0600

    Arreglando Registro Cobro

--- SegundoParcial_AP/BLL/ClientesBLL.cs
+++ SegundoParcial_AP/BLL/ClientesBLL.cs
@@ -1,13 +1,34 @@
-﻿using System;
+﻿using SegundoParcial_AP.DAL;
+using SegundoParcial_AP.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace SegundoParcial_AP.BLL
 {
     public class ClientesBLL
     {
+        public static List<Clientes> GetList(Expression<Func<Clientes, bool>> cliente)
+        {
+            List<Clientes> Lista = new List<Clientes>();
+            Contexto contexto = new Contexto();
 
+            try
+            {
+                Lista = contexto.Clientes.Where(cliente).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return Lista;
+        }
 
     }
 }
